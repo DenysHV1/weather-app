25218f020742d9def892f7ecbcbcb7dc2f5f7b3d
DenVik <denyagarkusha2@gmail.com>
2025-12-01 17:38:05 +0100

    last fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { useEffect } from "react";
 import { Container } from "./components/container/Container";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import type { AppDispatch } from "./redux/store";
 import { getWeatherByPosition } from "./redux/weather/thunks";
 import { useGeolocation } from "./hooks/useGeolocation";
 import MainWidget from "./components/MainWidget/MainnWidget";
 import SearchWeather from "./components/SearchWeather/SearchWeather";
 import CityHistory from "./components/CityHistory/CityHistory";
-import ErrorComponent from "./components/ErrorComponent/ErrorComponent";
-import { selectCurrent } from "./redux/weather/selectors";
 
 function App() {
   const { position, error, getLocation } = useGeolocation();
   const dispatch = useDispatch<AppDispatch>();
-  const current = useSelector(selectCurrent);
 
   useEffect(() => {
     getLocation();
@@ -26,17 +23,8 @@ function App() {
     );
   }, [position, dispatch]);
 
-  if (error || !current) {
-    return (
-      <Container>
-        <SearchWeather />
-        <main className="main">
-          <ErrorComponent />
-          <CityHistory />
-        </main>
-        {error && <div className="geo-error">{error}</div>}
-      </Container>
-    );
+  if (error) {
+    console.error(error);
   }
 
   return (
